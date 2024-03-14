02b9b68005c588b496d992d21eec6f971093921a
csohb <contact.youjinkwon@gmail.com>
2024-03-14 21:28:23 +0900

    Time: 77 ms (5.41%), Space: 7.1 MB (56.11%) - LeetHub

--- 0155-min-stack/0155-min-stack.go
+++ 0155-min-stack/0155-min-stack.go
@@ -1,15 +1,24 @@
 type MinStack struct {
     stack []int
+    minStack []int
 }
 
 
 func Constructor() MinStack {
-    return MinStack{}
+    return MinStack{
+        stack : make([]int, 0, 30000),
+        minStack : make([]int, 0, 30000),
+    }
 }
 
 
 func (this *MinStack) Push(val int)  {
+    min := val
+    if len(this.minStack) > 0 && this.minStack[len(this.minStack) - 1] < min {
+        min = this.minStack[len(this.minStack) -1 ]
+    }
     this.stack = append(this.stack, val)
+    this.minStack = append(this.minStack, min)
 }
 
 
