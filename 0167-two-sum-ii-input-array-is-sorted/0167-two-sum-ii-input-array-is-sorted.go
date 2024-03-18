da26a1a37338933dfe0a37420f9eb3b4bb2a0e74
csohb <contact.youjinkwon@gmail.com>
2024-03-18 11:41:44 +0900

    Time: 735 ms (5.01%), Space: 6.3 MB (10.97%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -1,7 +1,7 @@
 func twoSum(numbers []int, target int) []int {
     // two sum ... 
 
-    var answer []int
+    answer := make([]int, 0, 2)
     for i, v := range numbers {
         for j, vv := range numbers {
             if i == j {
@@ -9,7 +9,6 @@ func twoSum(numbers []int, target int) []int {
             }
 
             if v + vv == target {
-                //fmt.Println("v:", v, ",vv:",vv)
                 answer = append(answer, i+1)
                 answer = append(answer, j+1)
                 return answer
