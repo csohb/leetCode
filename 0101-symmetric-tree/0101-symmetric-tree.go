c706f0d035b96625250b2845adb38cdb92890f30
csohb <contact.youjinkwon@gmail.com>
2024-03-30 00:29:20 +0900

    Time: 3 ms (63.04%), Space: 3 MB (22.23%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    return isSymm(root.Left, root.Right)
+}
+
+func isSymm(left *TreeNode, right *TreeNode) bool {
+    if left == nil && right == nil {
+        return true
+    }
+
+    if left == nil || right == nil || left.Val != right.Val {
+        return false
+    }
+
+    return isSymm(left.Left, right.Right) && isSymm(left.Right, right.Left)
+}
+ 
