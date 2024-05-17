1c8555a10a8c52adb89f85c7acc9757341ab92c9
csohb <contact.youjinkwon@gmail.com>
2024-05-17 17:47:38 +0900

    Time: 7 ms (41.15%), Space: 5.4 MB (7.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,25 +1,14 @@
 func twoSum(nums []int, target int) []int {
     // two numbers 
-    var result []int 
+    var result []int
+    targetMap := make(map[int]int)
 
-    // target 에다가 하나씩 빼면서 0이 되는지 체크 
-    // map ... index, value 추가 
-
-    sumMap := make(map[int]int)
     for i, v := range nums {
-        sumMap[i] = v
-    }
-
-
-    for k, v := range sumMap {
-        for k2, v2 := range sumMap {
-            if k2 == k {
-                continue
-            }
-            if v + v2 == target {
-                result = []int{k, k2}
-            }
+        diff := target - v
+        if vv, ok := targetMap[diff]; ok {
+            result = []int{i, vv}
         }
+        targetMap[v] = i
     }
 
     return result
