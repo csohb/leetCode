5139faad17e20c58c77b51a036486a8e7f3b238f
csohb <contact.youjinkwon@gmail.com>
2025-04-06 11:46:38 +0900

    Time: 2 ms (43.22%), Space: 5 MB (40.25%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.go
+++ 0043-multiply-strings/0043-multiply-strings.go
@@ -1,39 +1,78 @@
 func multiply(num1 string, num2 string) string {
+    // if num1 == "0" || num2 == "0" {
+    //     return "0"
+    // }
+
+    // resultArr := make([]int, len(num1)+len(num2))
+
+    // for i:=len(num1)-1; i>=0; i-- {
+    //     for j:=len(num2)-1; j>=0; j-- {
+    //         // 12 * 34
+    //         product := int(num1[i] - '0') * int(num2[j]- '0')
+
+    //         position := i + j + 1
+    //         carryPosition := i + j
+
+    //         sum := resultArr[position] + product
+
+    //         resultArr[position] = sum % 10
+
+    //         resultArr[carryPosition]+= sum / 10
+
+    //         //fmt.Println("resultArr:",resultArr) 
+    //     }
+    // }
+
+    // res := ""
+    // idx := 0
+
+    // for idx < len(resultArr) && resultArr[idx] == 0 {
+    //     idx++
+    // }
+
+    // for i:=idx; i < len(resultArr); i++ {
+    //     res += strconv.Itoa(resultArr[i])
+    // }
+
+
+    // return res
+
+
     if num1 == "0" || num2 == "0" {
         return "0"
     }
 
-    resultArr := make([]int, len(num1)+len(num2))
 
-    for i:=len(num1)-1; i>=0; i-- {
-        for j:=len(num2)-1; j>=0; j-- {
-            // 12 * 34
-            product := int(num1[i] - '0') * int(num2[j]- '0')
+    res := make([]int, len(num1)+len(num2))
+
+    for i:= len(num1)-1; i>=0; i-- {
+        for j:= len(num2)-1; j>=0; j-- {
+            product := int(num1[i] - '0') * int(num2[j] - '0')
 
             position := i + j + 1
             carryPosition := i + j
 
-            sum := resultArr[position] + product
+            sum := res[position] + product
 
-            resultArr[position] = sum % 10
+            res[position] = sum % 10
+            res[carryPosition] += sum / 10 
 
-            resultArr[carryPosition]+= sum / 10
-
-            //fmt.Println("resultArr:",resultArr) 
+            //fmt.Println("res:",res)
         }
     }
 
-    res := ""
     idx := 0
+    resStr := ""
 
-    for idx < len(resultArr) && resultArr[idx] == 0 {
+    //[0 5 6 0 8 8]
+
+    for idx < len(res) && res[idx] == 0 {
         idx++
     }
 
-    for i:=idx; i < len(resultArr); i++ {
-        res += strconv.Itoa(resultArr[i])
+    for i := idx; i < len(res); i++ {
+        resStr += strconv.Itoa(res[i])
     }
 
-
-    return res
+    return resStr
 }
