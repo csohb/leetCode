aead3b4650c93508189813469d84d17ba53e2dae
csohb <contact.youjinkwon@gmail.com>
2025-04-07 13:15:25 +0900

    Time: 0 ms (100%), Space: 4.1 MB (66.86%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -6,53 +6,71 @@
  *     Right *TreeNode
  * }
  */
-
- import (
-     "fmt"
-     "reflect"
- )
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-    var answer bool
-
     if p == nil && q == nil {
         return true
     }
 
-    pList := bfs(p)
-    qList := bfs(q)
-
-    if reflect.DeepEqual(pList, qList) {
-        answer = true
-    } else {
-        answer = false
+    if p == nil || q == nil {
+        return false
     }
 
-    return answer   
-}
+    // check if value is identical
 
-func bfs(root *TreeNode) []*TreeNode {
-    if root == nil {
-        return nil
+    if p.Val != q.Val {
+        return false
     }
-    queue := make([]*TreeNode, 0)
-    queue = append(queue, root)
 
-    nodeList := make([]*TreeNode, 0)
-    for len(queue) > 0 {
-        start := queue[0]
-        queue = queue[1:]
-        nodeList = append(nodeList, start)
+    queue1 := []*TreeNode{p}
+    queue2 := []*TreeNode{q}
 
-        if start.Left != nil {
-            queue = append(queue, start.Left)
-            nodeList = append(nodeList, start.Left)
+    for len(queue1) > 0 {
+
+        level := len(queue1)
+        for i:= 0; i < level; i++ {
+            curr1 := queue1[0]
+            curr2 := queue2[0]
+
+            queue1 = queue1[1:]
+            queue2 = queue2[1:]
+
+            if (curr1.Left == nil) != (curr2.Left == nil) {
+                return false
+            }
+        
+            if (curr1.Right == nil) != (curr2.Right == nil) {
+                return false
+            }
+
+
+            // fmt.Println("curr1.Left:",curr1.Left.Val)
+            // //fmt.Println("curr2.Left:",curr2.Left.Val)
+            // fmt.Println("curr1.Right:",curr1.Right.Val)
+            // fmt.Println("curr2.Right:",curr2.Right.Val)
+
+            // if curr1.Left != curr2.Left || curr1.Right != curr2.Right {
+                
+            //     return false
+            // }
+
+            if curr1.Left != nil {
+                if curr1.Left.Val != curr2.Left.Val {
+                    return false
+                }
+                queue1 = append(queue1, curr1.Left)
+                queue2 = append(queue2, curr2.Left)
+            }
+            
+            if curr1.Right != nil {
+                if curr1.Right.Val != curr2.Right.Val {
+                    return false
+                }
+                queue1 = append(queue1, curr1.Right)
+                queue2 = append(queue2, curr2.Right)
+            }
+
         }
 
-        if start.Right != nil {
-            queue = append(queue, start.Right)
-            nodeList = append(nodeList, start.Right)
-        }
     }
-
-    return nodeList
+    return true
 }
