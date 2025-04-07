96c1b686c3c448c0f36c29a234ea1f1d64e4cfdf
csohb <contact.youjinkwon@gmail.com>
2025-04-07 12:54:27 +0900

    Time: 0 ms (100%), Space: 4.1 MB (56.32%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.go
+++ 0226-invert-binary-tree/0226-invert-binary-tree.go
@@ -7,28 +7,32 @@
  * }
  */
 func invertTree(root *TreeNode) *TreeNode {
-    // left <-> right
+    // BFS
+    // switch in each level of tree
     if root == nil {
         return nil
     }
 
-    queue := make([]*TreeNode, 0)
-    queue = append(queue, root)
+    queue := []*TreeNode{root}
 
     for len(queue) > 0 {
-        start := queue[0]
-        queue = queue[1:]
+        level := len(queue)
 
-        start.Left, start.Right = start.Right, start.Left
-        if start.Left != nil {
-            queue = append(queue, start.Left)
-        } 
+        for i:=0; i < level; i++ {
+            curr := queue[0]
+            queue = queue[1:]
 
-        if start.Right != nil {
-            queue = append(queue, start.Right)
+            curr.Left, curr.Right = curr.Right, curr.Left
+
+            if curr.Left != nil {
+                queue = append(queue, curr.Left)
+            }
+
+            if curr.Right != nil {
+                queue = append(queue, curr.Right)
+            }
         }
     }
 
     return root
 }
-
