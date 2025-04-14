ac899be618fd5b3448c62714d5348ce4909f32f8
csohb <contact.youjinkwon@gmail.com>
2025-04-14 19:27:16 +0900

    Time: 14 ms (42.21%), Space: 9.7 MB (32.41%) - LeetHub

--- 0148-sort-list/0148-sort-list.go
+++ 0148-sort-list/0148-sort-list.go
@@ -5,57 +5,110 @@
  *     Next *ListNode
  * }
  */
-func sortList(head *ListNode) *ListNode {
-    if head == nil {
-        return nil
-    }
+
+ // with heap sort
+// func sortList(head *ListNode) *ListNode {
+//     if head == nil {
+//         return nil
+//     }
     
 
-    hp := &ListHeap{}
-    heap.Init(hp)
+//     hp := &ListHeap{}
+//     heap.Init(hp)
 
-    for head != nil {
-        next := head.Next
-        head.Next = nil
-        heap.Push(hp, head)
-        head = next
+//     for head != nil {
+//         next := head.Next
+//         head.Next = nil
+//         heap.Push(hp, head)
+//         head = next
+//     }
+
+//     newList := &ListNode{}
+//     current := newList
+
+//     for hp.Len() > 0 {
+//         node := heap.Pop(hp).(*ListNode)
+//         current.Next = node
+//         current = node 
+//     }
+
+//     return newList.Next
+// }
+
+
+// type ListHeap []*ListNode
+
+// func(h ListHeap) Len() int {
+//     return len(h)
+// }
+
+// func(h ListHeap) Less(i, j int) bool {
+//     return h[i].Val < h[j].Val
+// }
+
+// func(h ListHeap) Swap(i, j int) {
+//     h[i], h[j] = h[j], h[i]
+// } 
+
+// func(h *ListHeap) Push(x interface{}) {
+//     *h = append(*h, x.(*ListNode))
+// } 
+
+// func(h *ListHeap) Pop() interface{} {
+//     old := *h
+//     n := len(old)
+//     x := old[n-1]
+//     *h = old[:n-1]
+//     return x
+// }
+
+func sortList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
     }
 
-    newList := &ListNode{}
-    current := newList
+    mid := getMid(head)
+    left := sortList(head)
+    right := sortList(mid)
 
-    for hp.Len() > 0 {
-        node := heap.Pop(hp).(*ListNode)
-        current.Next = node
-        current = node 
+    return merge(left, right)
+}
+
+func getMid(head *ListNode) *ListNode {
+    slow, fast := head, head
+    var prev *ListNode
+
+    for fast != nil && fast.Next != nil {
+        prev = slow
+        slow = slow.Next
+        fast = fast.Next.Next
     }
 
-    return newList.Next
+    prev.Next = nil
+
+    return slow 
 }
 
+func merge(left, right *ListNode) *ListNode {
+    dummy := &ListNode{}
+    current := dummy
 
-type ListHeap []*ListNode
+    for left != nil && right != nil {
+        if left.Val < right.Val {
+            current.Next = left
+            left = left.Next
+        } else {
+            current.Next = right
+            right = right.Next
+        }
+        current = current.Next
+    }
 
-func(h ListHeap) Len() int {
-    return len(h)
-}
+    if left != nil {
+        current.Next = left
+    } else {
+        current.Next = right
+    }
 
-func(h ListHeap) Less(i, j int) bool {
-    return h[i].Val < h[j].Val
-}
-
-func(h ListHeap) Swap(i, j int) {
-    h[i], h[j] = h[j], h[i]
-} 
-
-func(h *ListHeap) Push(x interface{}) {
-    *h = append(*h, x.(*ListNode))
-} 
-
-func(h *ListHeap) Pop() interface{} {
-    old := *h
-    n := len(old)
-    x := old[n-1]
-    *h = old[:n-1]
-    return x
+    return dummy.Next
 }
