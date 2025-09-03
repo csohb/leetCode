5058ee758b425e97ed7de94463bb6ad9ea23ad4f
csohb <contact.youjinkwon@gmail.com>
2025-09-03 22:50:46 +0200

    Time: 3 ms (5.85%), Space: 8.5 MB (75.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,8 +1,7 @@
 func reverseString(s []byte)  {
-    // ^^;;
-    half := len(s) / 2
+    half := len(s) / 2 
     start := 0
-    end := 1
+    end := 1 
     for half > 0 {
         s[start], s[len(s)-end] = s[len(s)-end], s[start]
         start++
