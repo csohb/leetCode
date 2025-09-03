d74d8af50d3b0aea2adfacd49aee1c0878b56ce0
csohb <contact.youjinkwon@gmail.com>
2025-09-03 22:29:48 +0200

    Time: 17 ms (84.85%), Space: 9.4 MB (48.55%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.go
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.go
@@ -1,38 +1,22 @@
 func kClosest(points [][]int, k int) [][]int {
+    // *Minimum or Closest -> Min Heap
     res := make([][]int, 0)
 
-    // i < k --> append to List (sort), append to res
-
-    // array
-    // dList := make([][]int, 0)
-
-    // for _, point := range points {
-    //     distance := (point[0] * point[0]) + (point[1] * point[1])
-    //     dList = append(dList, []int{distance, point[0], point[1]})        
-    // }
-
-    // sort.Slice(dList, func(i, j int) bool{
-    //     return dList[i][0] < dList[j][0]
-    // })
-
-    // for i:=k-1; i>=0; i-- {
-    //     x := dList[i][1]
-    //     y := dList[i][2]
-    //     res = append(res, []int{x,y})
-    // }
-
-    minHeap := &MinHeap{}
-    heap.Init(minHeap)
+    mHeap := &MinHeap{}
+    heap.Init(mHeap)
 
     for _, point := range points {
-        x, y := point[0], point[1]
-        distance := (x * x) + (y * y)
-        heap.Push(minHeap, Point{distance:distance, x:x, y:y})
+        x := point[0]
+        y := point[1]
+        distance := (x*x) + (y*y)
+
+        heap.Push(mHeap, Point{distance, x, y})
     }
 
+
     for k > 0 {
-        point := heap.Pop(minHeap).(Point)
-        res = append(res, []int{point.x, point.y})
+        point := heap.Pop(mHeap).(Point)
+        res = append(res, []int{point.x,point.y})
         k--
     }
 
@@ -47,23 +31,23 @@ type Point struct {
 
 type MinHeap []Point
 
-func (h MinHeap) Len() int {
+func(h MinHeap) Len() int {
     return len(h)
 }
 
-func (h MinHeap) Less(i, j int) bool {
+func(h MinHeap) Less(i, j int) bool {
     return h[i].distance < h[j].distance
 }
 
-func (h MinHeap) Swap(i, j int) {
+func(h MinHeap) Swap(i, j int) {
     h[i], h[j] = h[j], h[i]
 }
 
-func (h *MinHeap) Push(x interface{}) {
+func(h *MinHeap) Push(x interface{}) {
     *h = append(*h, x.(Point))
 }
 
-func (h *MinHeap) Pop() interface{} {
+func(h *MinHeap) Pop() interface{} {
     old := *h
     n := len(old)
     x := old[n-1]
