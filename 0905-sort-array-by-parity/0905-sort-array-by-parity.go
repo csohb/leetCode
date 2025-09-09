91192f34db5bfbadea7421003b5589f675ae0b48
csohb <contact.youjinkwon@gmail.com>
2025-09-09 19:35:17 +0200

    Time: 0 ms (100%), Space: 6.5 MB (29.41%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.go
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.go
@@ -9,25 +9,42 @@ func sortArrayByParity(nums []int) []int {
     // even = append(even, odd)
 
 
-    evenArr := make([]int, 0)
-    oddArr := make([]int, 0)
+    // evenArr := make([]int, 0)
+    // oddArr := make([]int, 0)
 
-    for _, num := range nums {
-        if num % 2 == 0 {
-            // even
-            evenArr = append(evenArr, num)
-        } else {
-            oddArr = append(oddArr, num)
+    // for _, num := range nums {
+    //     if num % 2 == 0 {
+    //         // even
+    //         evenArr = append(evenArr, num)
+    //     } else {
+    //         oddArr = append(oddArr, num)
+    //     }
+    // }
+
+    // //evenArr = append(evenArr, oddArr)
+
+    // for _, oddNum := range oddArr {
+    //     evenArr = append(evenArr, oddNum)
+    // }
+
+
+    // return evenArr
+
+    right := 0
+    left := len(nums) - 1
+
+    for right < left {
+        if nums[right] % 2 == 0 {
+            right++
+        } else if nums[left] % 2 == 1 {
+            left--
+        } else if nums[right] % 2 == 1 && nums[left] % 2 == 0 {
+            nums[right], nums[left] = nums[left], nums[right]
+            right++
+            left--
         }
     }
 
-    //evenArr = append(evenArr, oddArr)
-
-    for _, oddNum := range oddArr {
-        evenArr = append(evenArr, oddNum)
-    }
-
-
-    return evenArr
+    return nums
 
 }
