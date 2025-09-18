345bab3fd362ab8b3327e48f73cf71e4f2cb7e95
csohb <contact.youjinkwon@gmail.com>
2025-09-18 22:42:47 +0200

    Time: 11 ms (6.08%), Space: 4.8 MB (38.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,17 +1,30 @@
 func isAnagram(s string, t string) bool {
+    // store alphabet in hashMap and compare if it's same or not
     if len(s) != len(t) {
         return false
     }
+
+    // 2 hashMap
     sMap := make(map[byte]int)
     tMap := make(map[byte]int)
+    for i:=0; i<len(s); i++ {
+        if _, ok := sMap[byte(s[i])]; ok {
+            sMap[byte(s[i])]++
+        } else {
+            sMap[byte(s[i])] = 1
+        }
+    }
 
-    for i:=0; i < len(s); i++ {
-        sMap[s[i]]++
-        tMap[t[i]]++
+    for i:=0; i<len(t); i++ {
+        if _, ok := tMap[byte(t[i])]; ok {
+            tMap[byte(t[i])]++
+        } else {
+            tMap[byte(t[i])] = 1
+        }
     }
 
     for k, v := range sMap {
-        if tMap[k] != v {
+        if v != tMap[k] {
             return false
         }
     }
