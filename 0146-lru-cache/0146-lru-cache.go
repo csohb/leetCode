39334d0766bdf5c10a10912e7a93f5c8fc5bf925
csohb <contact.youjinkwon@gmail.com>
2025-09-18 21:25:30 +0200

    Time: 85 ms (66.46%), Space: 86.2 MB (14.62%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.go
+++ 0146-lru-cache/0146-lru-cache.go
@@ -1,78 +1,97 @@
+// hashmap : store data key - value / when search hashmap -> time complexicty O(1) constant time
+// Double Linked List : store the order, but order needs to be updated or the node deleted -> change of the order 
+// order should be linked to each other
+// head(most recent) (  , , ,  ) tail (least)
+// maintain time complexity as O(1)
+type Node struct {
+    key int
+    val int
+    prev *Node
+    next *Node
+}
+
 
 type LRUCache struct {
-    head, tail *listNode
-    hMap map[int]*listNode
     capacity int
-}
-
-func newLRUCache(head, tail *listNode, cap int) LRUCache {
-    return LRUCache{
-        head: head,
-        tail: tail,
-        hMap: make(map[int]*listNode),
-        capacity: cap,
-    }
-}
-
-type listNode struct {
-    prev *listNode
-    next *listNode
-    key, val int
-}
-
-func newListNode(key, val int) *listNode {
-    return &listNode{
-        key: key,
-        val: val,
-    }
+    hMap map[int]*Node
+    head *Node
+    tail *Node
 }
 
 
 func Constructor(capacity int) LRUCache {
-    head, tail := newListNode(0,0), newListNode(0,0)
+    head := &Node{}
+    tail := &Node{}
     head.next = tail
     tail.prev = head
-    return newLRUCache(head, tail, capacity)
+    return LRUCache{
+        capacity: capacity, 
+        hMap: make(map[int]*Node, capacity),
+        head: head,
+        tail: tail,
+    }
 }
 
 
 func (this *LRUCache) Get(key int) int {
-    if n, ok := this.hMap[key]; ok {
-        this.remove(n)
-        this.insert(n)
-        return n.val
+    // lookup hMap[key] -> pointing Node, value (return)
+    // Get key -> current Node should be go next to Head
+    node, ok := this.hMap[key]
+    if !ok {
+        return -1
     }
+    // disconnect from current prev, next 
+    this.delete(node)
+    // put next to head 
+    this.addNextHead(node)
 
-    return -1
+    return node.val
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
-    if _, ok := this.hMap[key]; ok {
-        this.remove(this.hMap[key])
+    // check if this.capacity will be over after this put operation
+    if node, ok := this.hMap[key]; ok {
+        node.val = value
+        this.delete(node)
+        this.addNextHead(node)
+        return
     }
 
-    if len(this.hMap) == this.capacity {
-        this.remove(this.tail.prev)
-    }
-
-    this.insert(newListNode(key, value))
+    if len(this.hMap) ==  this.capacity {
+        // delete least used Node 
+        // least used Node == tail.prev 
+        lru := this.tail.prev
+        this.delete(lru)
+        delete(this.hMap, lru.key)
+    } 
+    // put new Node
+    newNode := &Node{key: key, val: value}
+    this.addNextHead(newNode)
+    this.hMap[key] = newNode
 }
 
-
-func(this *LRUCache) remove(node *listNode) {
-    delete(this.hMap, node.key)
+func (this *LRUCache) delete(node *Node) {
+    if node == nil || node == this.head || node == this.tail {
+        return
+    }
     node.prev.next = node.next
     node.next.prev = node.prev
+
+    node.prev = nil
+    node.next = nil
+    // -> node lost the conneciton between prev & next
 }
 
-func(this *LRUCache) insert(node *listNode) {
-    this.hMap[node.key] = node
-    next := this.head.next
+func (this *LRUCache) addNextHead(node *Node) {
+    // put inbetween head // head.next
+    old := this.head.next
+
     this.head.next = node
     node.prev = this.head
-    next.prev = node
-    node.next = next
+    node.next = old
+
+    old.prev = node
 }
 
 /**
