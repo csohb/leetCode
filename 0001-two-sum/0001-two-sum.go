978c84e18969e3348e031394090762c3e64707ba
csohb <contact.youjinkwon@gmail.com>
2025-09-18 23:18:32 +0200

    Time: 29 ms (10.64%), Space: 5.3 MB (99.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -6,8 +6,6 @@ func twoSum(nums []int, target int) []int {
 
         for j:=i+1; j<len(nums); j++ {
             if sum + nums[j] == target {
-                fmt.Println("i:",nums[i])
-                fmt.Println("j:",nums[j])
                 res = append(res, i)
                 res = append(res, j)
             }
