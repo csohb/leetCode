409107348b6da5d44a1a4d8586bd1b3e2593ca65
csohb <contact.youjinkwon@gmail.com>
2025-09-20 23:24:22 +0200

    Time: 0 ms (100%), Space: 4.3 MB (83.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.go
@@ -6,24 +6,26 @@
  * }
  */
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-    start := &ListNode{}
-    current := start
-    // 일단 추가한다음 뭐가 더 크면 순서 바꾸기?
-    for list1 != nil && list2 != nil  {
-        if list1.Val < list2.Val {
-            current.Next = list1
-            list1= list1.Next
+    newList := &ListNode{}
+    tail := newList
+
+    for list1 != nil && list2 != nil {
+        // compare list value
+        if list1.Val <= list2.Val {
+            tail.Next = list1
+            list1 = list1.Next
         } else {
-            current.Next = list2
+            tail.Next = list2
             list2 = list2.Next
         }
-        current = current.Next
-     }
+        tail = tail.Next
+    }
 
-     if list1 == nil {
-        current.Next = list2
-     } else {
-        current.Next = list1
-     }
-    return start.Next 
+    if list1 != nil {
+        tail.Next = list1
+    } else {
+        tail.Next = list2
+    }
+
+    return newList.Next
 }
