aaf243feb9ce7a531b30aa20f062d34f0efe5b78
csohb <contact.youjinkwon@gmail.com>
2025-09-21 17:38:58 +0200

    Time: 3 ms (60.08%), Space: 4.4 MB (61.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,52 +1,30 @@
 func longestPalindrome(s string) string {
-    // sliding window
-    // to be palindromic, at least start == end
+    res := ""
+    resLen := 0
 
-    left := 0
-    right := 0
-
-    maxLen := 0 
-    result := ""
-for left < len(s) {
-        right = left // 각 시작점마다 right을 left로 초기화
-        
-        // right을 증가시키며 윈도우 크기 확장
-        for right < len(s) {
-            // 현재 윈도우가 팰린드롬인지 확인
-            if checkPalindromic(s[left : right+1]) {
-                curLen := right - left + 1
-                
-                // 더 긴 팰린드롬을 찾았다면 업데이트
-                if curLen > maxLen {
-                    maxLen = curLen
-                    result = s[left : right+1]
-                }
+    for i, _ := range s {
+        // odd case
+        left, right := i, i 
+        for left >= 0 && right < len(s) && s[left] == s[right] {
+            if right - left + 1 > resLen {
+                res = s[left:right+1]
+                resLen = right - left + 1
             }
-            
-            right++ // 윈도우 오른쪽 확장
+            left--
+            right++
         }
-        
-        left++ // 다음 시작점으로 이동
-    }
 
-    if maxLen == 0 {
-        return s[:1]
+        // even case
+        left, right = i, i+1 
+        for left >= 0 && right < len(s) && s[left] == s[right] {
+            if right - left + 1 > resLen {
+                res = s[left:right+1]
+                resLen = right - left + 1
+            }
+            left--
+            right++
+        }
     }
-
-    return result
+    return res
 }
 
-
-func checkPalindromic(s string) bool {
-    left := 0
-    right := len(s) -  1
-
-    for left < right {
-        if s[left] != s[right] {
-            return false
-        }
-        left++
-        right--
-    }
-    return true
-}
