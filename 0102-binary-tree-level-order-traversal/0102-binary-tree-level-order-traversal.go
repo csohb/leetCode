6c4f370f3e65207c97886134aa243bfe2be896b2
csohb <contact.youjinkwon@gmail.com>
2025-09-22 14:35:47 +0200

    Time: 0 ms (100%), Space: 5.6 MB (62.65%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
@@ -7,38 +7,29 @@
  * }
  */
 func levelOrder(root *TreeNode) [][]int {
-    // bfs
-    // each level
-    // queue
-
     if root == nil {
         return nil
     }
-
     res := [][]int{}
+
     queue := []*TreeNode{root}
 
     for len(queue) > 0 {
-        // level size
         size := len(queue)
-        level := []int{} // each element of res array
-
+        level := []int{}
         for i:=0; i<size; i++ {
             n:= queue[0]
-        queue = queue[1:]
+            queue = queue[1:]
 
+            if n.Left != nil {
+                queue = append(queue, n.Left)
+            }
 
-        if n.Left != nil {
-            queue = append(queue, n.Left)
+            if n.Right != nil {
+                queue = append(queue, n.Right)
+            }
+            level = append(level, n.Val)
         }
-
-        if n.Right != nil {
-            queue = append(queue, n.Right)
-        }
-
-        level = append(level, n.Val)
-        }
-
         res = append(res, level)
     }
 
